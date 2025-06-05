4d91579229b27021b8356b468ff57e57afaf751e
cecilialbry <159028619+cecilialbry@users.noreply.github.com>
2025-06-05 15:24:15 +0200

    Update semaine9.py

--- semaine9.py
+++ semaine9.py
@@ -153,7 +153,7 @@ vehicle_options = {
 }
 # === Fonction de simulation simplifiée (colle ta vraie fonction ici) ===
 def run_simulation(country, month, profile_name, arrival_hour, departure_hour,
-                   initial_soc, target_soc, num_vehicles, mode, vehicle_type, peak_power_kwp):
+    initial_soc, target_soc, num_vehicles, mode, vehicle_type, peak_power_kwp):
     battery_capacity_kWh = 70
     max_kWh_per_hour = 11
     min_soc_ratio = 0.20
